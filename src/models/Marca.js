3b0e884905b60316e720a1db62404d0488864414
mvgarc <mariav2601@gmail.com>
2025-05-11 11:41:44 -0400

    feat: agregar modelo Marca con definición de atributos y configuración de tabla

---
+++ src/models/Marca.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const Marca = sequelize.define('Marca', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    logo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    }, {
+    tableName: 'marcas', // El nombre de la tabla en la BD
+    timestamps: false,
+});
+
+module.exports = Marca;
