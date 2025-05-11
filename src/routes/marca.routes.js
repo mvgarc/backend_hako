35bd9e3ddc9dde12ff19cd17aa230afd27859cca
mvgarc <mariav2601@gmail.com>
2025-05-11 09:46:20 -0400

    feat: agregar soporte para subir archivos en la ruta de creación de marcas

--- src/routes/marca.routes.js
+++ src/routes/marca.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const marcaController = require('../controllers/marca.controller');
+const multer = require('multer');
+const path = require('path');
+
+// Configuración de Multer para subir archivos
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // Rutas CRUD para Marcas
-router.post('/', marcaController.createMarca);
+router.post('/', upload.single('logo'), marcaController.createMarca);
 router.get('/', marcaController.getMarcas);
 router.delete('/:id', marcaController.deleteMarca);
 
