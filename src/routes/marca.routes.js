cd2c9f13178e9fc725ef3bb8953400b924b58953
mvgarc <mariav2601@gmail.com>
2025-05-11 11:39:18 -0400

    feat: eliminar configuración de Multer y simplificar rutas CRUD para marcas

--- src/routes/marca.routes.js
+++ src/routes/marca.routes.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const marcaController = require('../controllers/marca.controller');
-const multer = require('multer');
-const path = require('path');
 
-// Configuración de Multer para subir archivos
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
-
-// Rutas CRUD para Marcas
-router.post('/', upload.single('logo'), marcaController.createMarca);
+// Rutas
 router.get('/', marcaController.getMarcas);
+router.post('/', marcaController.createMarca);
+router.get('/:id', marcaController.getMarcaById);
+router.put('/:id', marcaController.updateMarca);
 router.delete('/:id', marcaController.deleteMarca);
 
 module.exports = router;
