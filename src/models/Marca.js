09a4b493b5f8064624902416da468116e13cb4df
mvgarc <mariav2601@gmail.com>
2025-05-30 14:50:46 -0400

    fix: import Catalogo and define relationship in Marca model

--- src/models/Marca.js
+++ src/models/Marca.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Catalogo = require('./Catalogo'); // ¡IMPORTANTE! Solo importa si la necesitas para la relación inversa aquí
 
 const Marca = sequelize.define('Marca', {
     id: {
@@ -17,7 +18,10 @@ const Marca = sequelize.define('Marca', {
     },
     }, {
     tableName: 'marcas', // El nombre de la tabla en la BD
-    timestamps: false,
+    timestamps: false, // Mantienes esto en false
 });
+
+// Define la relación aquí si una Marca puede tener muchos Catalogos
+Marca.hasMany(Catalogo, { foreignKey: 'marcaId' });
 
 module.exports = Marca;
